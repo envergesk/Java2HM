68ff6e1c8bf9167f5af0f70c3ce310412c5d5c03
envergesk <ru.certificate.plus@gmail.com>
2022-05-11 10:07:08 +0300

    HomeWork4 done

--- ClientChat/src/main/java/com/example/clientchat/ClientChat.java
+++ ClientChat/src/main/java/com/example/clientchat/ClientChat.java
@@ -1,4 +1,5 @@
 package com.example.clientchat;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -6,13 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ClientChat extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientChat.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("ClientChat");
         stage.setScene(scene);
+
+        ClientController controller = fxmlLoader.getController();
+        controller.userList.getItems().addAll("user1", "user2");
+
         stage.show();
     }
 
